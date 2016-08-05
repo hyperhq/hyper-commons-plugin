abc57bc092b0922735deb0528a0ef32fbdf0deb4
Xianglin Gao <xlgao@zju.edu.cn>
2016-08-05 19:04:47 +0800

    add hyper installer
    Signed-off-by: Xianglin Gao <xlgao@zju.edu.cn>

---
+++ src/main/java/sh/hyper/plugins/hypercommons/tools/HyperTool.java
@@ -0,0 +1,139 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2015 Jesse Glick, CloudBees Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package sh.hyper.plugins.hypercommons.tools;
+
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.FilePath;
+import hudson.Util;
+import hudson.model.EnvironmentSpecific;
+import hudson.model.Node;
+import hudson.model.TaskListener;
+import hudson.slaves.NodeSpecific;
+import hudson.tools.ToolDescriptor;
+import hudson.tools.ToolInstallation;
+import hudson.tools.ToolInstaller;
+import hudson.tools.ToolProperty;
+import hudson.util.ArgumentListBuilder;
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import jenkins.model.Jenkins;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ * An installation of Docker.
+ * If nonempty, expected to have an executable file {@code bin/docker}.
+ * Use {@code <d:selectDockerTool field="toolName" xmlns:d="/lib/docker/commons/"/>} to customize.
+ */
+public class HyperTool extends ToolInstallation implements EnvironmentSpecific<HyperTool>, NodeSpecific<HyperTool> {
+
+    /**
+     * Unqualified Docker command name, for use in case no {@link HyperTool} has been selected and thus {@link #getExecutable} cannot be called.
+     */
+    static final String COMMAND = System.getProperty("HYPER_COMMAND", "hyper"); // property overridable only for tests
+
+    @DataBoundConstructor public HyperTool(String name, String home, List<? extends ToolProperty<?>> properties) {
+        super(name, home, properties);
+    }
+
+    /**
+     * Gets the executable name to use for a given launcher.
+     * Suitable for the first item in {@link ArgumentListBuilder}.
+     * @param name the name of the selected tool, or null for the default
+     * @param node optionally, a node (such as a slave) on which we are running Docker
+     * @param listener a listener, required in case {@code node} is not null
+     * @param env optionally, environment variables to use when expanding the home directory
+     * @return {@code docker} or an absolute path
+     */
+    public static @Nonnull String getExecutable(@CheckForNull String name, @CheckForNull Node node, @Nullable TaskListener listener, @CheckForNull EnvVars env) throws IOException, InterruptedException {
+        if (name != null) {
+            Jenkins j = Jenkins.getInstance();
+            if (j != null) {
+                for (HyperTool tool : j.getDescriptorByType(DescriptorImpl.class).getInstallations()) {
+                    if (tool.getName().equals(name)) {
+                        if (node != null) {
+                            tool = tool.forNode(node, listener);
+                        }
+                        if (env != null) {
+                            tool = tool.forEnvironment(env);
+                        }
+                        String home = Util.fixEmpty(tool.getHome());
+                        if (home != null) {
+                            if (node != null) {
+                                FilePath homeFP = node.createPath(home);
+                                if (homeFP != null) {
+                                    return homeFP.child("bin/hyper").getRemote();
+                                }
+                            }
+                            return home + "/bin/hyper";
+                        }
+                    }
+                }
+            }
+        }
+        return COMMAND;
+    }
+
+    public HyperTool forEnvironment(EnvVars environment) {
+        return new HyperTool(getName(), environment.expand(getHome()), getProperties());
+    }
+
+    public HyperTool forNode(Node node, TaskListener log) throws IOException, InterruptedException {
+            return new HyperTool(getName(), translateFor(node, log), getProperties().toList());
+    }
+
+    @Override public void buildEnvVars(EnvVars env) {
+        String home = Util.fixEmpty(getHome());
+        if (home != null) {
+            env.put("PATH+DOCKER", home + "/bin");
+        }
+    }
+
+    @Extension public static class DescriptorImpl extends ToolDescriptor<HyperTool> {
+
+        @Override public String getDisplayName() {
+            return "Hyper";
+        }
+
+        @Override public HyperTool[] getInstallations() {
+            load(); // TODO this ought to be automatic
+            return super.getInstallations();
+        }
+
+        @Override public void setInstallations(HyperTool... installations) {
+            super.setInstallations(installations);
+            save(); // TODO this ought to be automatic
+        }
+
+        @Override public List<? extends ToolInstaller> getDefaultInstallers() {
+            return super.getDefaultInstallers();
+        }
+
+    }
+
+}
